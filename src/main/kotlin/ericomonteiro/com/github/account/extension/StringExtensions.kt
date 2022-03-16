d6f9bbbe8b6aeb9af946709227ef7b4b9efa6bb6
Érico Monteiro <erico.cintra@gmail.com>
2022-03-16 03:11:19 -0300

    specify return for string extension

--- src/main/kotlin/ericomonteiro/com/github/account/extension/StringExtensions.kt
+++ src/main/kotlin/ericomonteiro/com/github/account/extension/StringExtensions.kt
@@ -2,4 +2,4 @@ package ericomonteiro.com.github.account.extension
 
 import com.google.gson.Gson
 
-fun <T> String.jsonToObject(target: Class<T>?) = Gson().fromJson(this, target)
+fun <T> String.jsonToObject(target: Class<T>?): T = Gson().fromJson(this, target)
