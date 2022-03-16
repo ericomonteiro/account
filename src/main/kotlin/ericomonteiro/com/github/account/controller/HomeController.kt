91de00094c6410b4cddd27426676206df91a3f8f
Érico Monteiro <erico.cintra@gmail.com>
2022-03-15 23:15:10 -0300

    first controller

---
+++ src/main/kotlin/ericomonteiro/com/github/account/controller/HomeController.kt
@@ -0,0 +1,20 @@
+package ericomonteiro.com.github.account.controller
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+@Controller("/home", produces = [MediaType.APPLICATION_JSON])
+class HomeController {
+
+    private val logger: Logger = LoggerFactory.getLogger(HomeController::class.java)
+
+    @Get
+    fun home(): Map<String, String> {
+        logger.info("passed here")
+        return mapOf(Pair("service", "up"))
+    }
+
+}
